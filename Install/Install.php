00eea0bc1c5d1047801c8a433c4a9075ec7398a9
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-02-06 03:51:39 +1100

    Initial Commit

---
+++ Install/Install.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Apps\Fintech\Packages\Test\Install;
+
+use Apps\Fintech\Packages\Test\Install\Schema\Test;
+use System\Base\BasePackage;
+use System\Base\Providers\ModulesServiceProvider\DbInstaller;
+
+class Install extends BasePackage
+{
+    protected $databases;
+
+    protected $dbInstaller;
+
+    public function init()
+    {
+        $this->database =
+            [
+                'fintech_test'  => [
+                    'schema'    => new \Apps\Fintech\Packages\Test\Install\Schema\Test,
+                    'model'     => new \Apps\Fintech\Packages\Test\Model\AppsFintechTest,
+                ],
+            ];
+
+        $this->dbInstaller = new DbInstaller;
+
+        return $this;
+    }
+
+    public function install()
+    {
+        $this->preInstall();
+
+        $this->installDb();
+
+        $this->postInstall();
+
+        return true;
+    }
+
+    protected function preInstall()
+    {
+        return true;
+    }
+
+    public function installDb()
+    {
+        $this->dbInstaller->installDb($this->databases);
+
+        return true;
+    }
+
+    public function postInstall()
+    {
+        //Do anything after installation.
+        return true;
+    }
+
+    public function truncate()
+    {
+        $this->dbInstaller->truncate($this->databases);
+    }
+
+    public function uninstall()
+    {
+        //Check Relationship
+        //Drop Table(s)
+        return true;
+    }
+}
